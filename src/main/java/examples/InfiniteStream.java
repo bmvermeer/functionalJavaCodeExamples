6909e2f074f1ca11bd34cff2c15ab12e76799556
brianvermeer <->
2019-03-17 20:59:14 +0300

    Order of operations: stream will run forever

--- src/main/java/examples/InfiniteStream.java
+++ src/main/java/examples/InfiniteStream.java
@@ -5,10 +5,10 @@ import java.util.stream.IntStream;
 public class InfiniteStream {
 
     public void execute() {
-        IntStream.iterate(0, i -> (i + 1) % 2)
-                .distinct()
-                .limit(10)
-                .forEach(System.out::println);
+        IntStream.iterate(0, i -> (i + 1) % 2) //0,1,0,1,0,1 etc.
+                .distinct() //0,1
+                .limit(10) //1-0, 2-1, 3-?
+                .forEach(System.out::println); //runs forever
     }
 
 
